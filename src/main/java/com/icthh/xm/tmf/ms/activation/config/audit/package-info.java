8eaafd44a750b8261d87773ee9ca6381913ebb18
Andrew Koidan <deathangel908@gmail.com>
2018-12-27 16:59:34 +0200

    Initial application generated by JHipster-5.3.4

---
+++ src/main/java/com/icthh/xm/tmf/ms/activation/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.icthh.xm.tmf.ms.activation.config.audit;
---
+++ src/main/java/com/icthh/xm/tmf/ms/activation/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.icthh.xm.tmf.ms.activation.web.rest;
