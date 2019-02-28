af29e72f906bdb9f4a72caefb6260ca1f218d446
ssenko <sergeysenja1992@gmail.com>
2019-02-28 15:28:12 +0200

    fix retry service

--- src/main/java/com/icthh/xm/tmf/ms/activation/service/RetryService.java
+++ src/main/java/com/icthh/xm/tmf/ms/activation/service/RetryService.java
@@ -9,7 +9,9 @@ import com.icthh.xm.tmf.ms.activation.events.EventsSender;
 import com.icthh.xm.tmf.ms.activation.repository.SagaEventRepository;
 import com.icthh.xm.tmf.ms.activation.utils.TenantUtils;
 import java.time.Instant;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +30,8 @@ public class RetryService {
     private final SagaEventRepository sagaEventRepository;
     private final TenantUtils tenantUtils;
 
+    private final Map<String, Boolean> scheduledEventsId = new ConcurrentHashMap<>();
+
     private RetryService self;
 
     @PostConstruct
@@ -37,11 +41,11 @@ public class RetryService {
 
     @Transactional
     public void rescheduleAllEvents() {
-        sagaEventRepository.findByStatus(ON_RETRY).forEach(eventsSender::resendEvent);
-        sagaEventRepository.findByStatus(WAIT_DEPENDS_TASK).forEach(eventsSender::resendEvent);
+        sagaEventRepository.findByStatus(ON_RETRY).forEach(this::doResend);
+        sagaEventRepository.findByStatus(WAIT_DEPENDS_TASK).forEach(this::doResend);
     }
 
-    public void retry(SagaEvent sagaEvent, SagaTaskSpec sagaTaskSpec) {
+    public void retry(SagaEvent sagaEvent, SagaTaskSpec sagaTaskSpec, SagaEvent.SagaEventStatus eventStatus) {
         int backOff = Math.min(sagaTaskSpec.getMaxBackOff(), sagaEvent.getBackOff() + sagaTaskSpec.getBackOff());
         sagaEvent.setBackOff(backOff);
         sagaEvent.setRetryNumber(sagaEvent.getRetryNumber() + 1);
@@ -52,29 +56,38 @@ public class RetryService {
             return;
         }
 
-        scheduleRetry(sagaEvent, ON_RETRY);
+        scheduleRetry(sagaEvent, eventStatus);
+    }
+
+    public void retry(SagaEvent sagaEvent, SagaTaskSpec sagaTaskSpec) {
+        retry(sagaEvent, sagaTaskSpec, ON_RETRY);
+    }
+
+    public void retryForWaitDependsTask(SagaEvent sagaEvent, SagaTaskSpec sagaTaskSpec) {
+        retry(sagaEvent, sagaTaskSpec, WAIT_DEPENDS_TASK);
     }
 
     private void scheduleRetry(SagaEvent sagaEvent, SagaEvent.SagaEventStatus eventStatus) {
         sagaEvent.setStatus(eventStatus);
         SagaEvent savedSagaEvent = sagaEventRepository.save(sagaEvent);
+
+        if (scheduledEventsId.containsKey(sagaEvent.getId())) {
+            log.warn("Event {} already scheduled", sagaEvent);
+            return;
+        }
+
         log.info("Schedule event {} for delay {}", savedSagaEvent, sagaEvent.getBackOff());
+        scheduledEventsId.put(sagaEvent.getId(), true);
         threadPoolTaskScheduler
             .schedule(() -> doResend(savedSagaEvent), Instant.now().plusSeconds(sagaEvent.getBackOff()));
     }
 
     public void doResend(SagaEvent sagaEvent) {
+        scheduledEventsId.remove(sagaEvent.getId());
         tenantUtils.doInTenantContext(() -> {
             try {
                 log.info("Retry event {}. Send into broker.", sagaEvent);
-
-                Optional<SagaEvent> actualSagaEvent = sagaEventRepository.findById(sagaEvent.getId());
-                if (actualSagaEvent.isPresent()) {
-                    eventsSender.sendEvent(actualSagaEvent.get());
-                    sagaEventRepository.delete(actualSagaEvent.get());
-                } else {
-                    log.warn("Event not found {}", sagaEvent);
-                }
+                self.removeAndSend(sagaEvent);
             } catch (Exception e) {
                 log.info("Error has happend.", e);
                 scheduleRetry(sagaEvent, ON_RETRY);
@@ -83,12 +96,20 @@ public class RetryService {
         }, sagaEvent.getTenantKey());
     }
 
+    @Transactional
+    public void removeAndSend(SagaEvent sagaEvent) {
+        Optional<SagaEvent> actualSagaEvent = sagaEventRepository.findById(sagaEvent.getId());
+        if (actualSagaEvent.isPresent()) {
+            eventsSender.sendEvent(actualSagaEvent.get());
+            sagaEventRepository.delete(actualSagaEvent.get());
+        } else {
+            log.warn("Event not found {}", sagaEvent);
+        }
+    }
+
     @Autowired
     public void setSelf(RetryService self) {
         this.self = self;
     }
 
-    public void retryForWaitDependsTask(SagaEvent sagaEvent, SagaTaskSpec taskSpec) {
-        scheduleRetry(sagaEvent, WAIT_DEPENDS_TASK);
-    }
 }
