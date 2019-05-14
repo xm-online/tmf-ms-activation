542ebf4595bab1bb6d9600f3bff810cf34a40d4e
Dmittry Tsarenko <dimatsarenko89@gmail.com>
2019-05-14 10:12:15 +0300

    add kafka event sender retry

--- src/test/java/com/icthh/xm/tmf/ms/activation/events/KafkaEventsSenderTest.java
+++ src/test/java/com/icthh/xm/tmf/ms/activation/events/KafkaEventsSenderTest.java
@@ -13,14 +13,12 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.binding.BinderAwareChannelResolver;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @EnableRetry
-@EnableBinding
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {KafkaEventsSender.class})
 public class KafkaEventsSenderTest {
