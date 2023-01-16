5490acca345425d42b9a437c74930894d7ff50de
vitalii.kyrychenko <vitalii.kyrychenko@jevera.software>
2023-01-16 16:07:44 +0100

    outbox

--- src/main/java/com/icthh/xm/tmf/ms/activation/config/DatabaseConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/activation/config/DatabaseConfiguration.java
@@ -39,8 +39,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@EnableJpaRepositories({"com.icthh.xm.tmf.ms.activation.repository",
-    "com.icthh.xm.commons.domainevent.outbox.repository"})
+@EnableJpaRepositories("com.icthh.xm.tmf.ms.activation.repository")
 @EnableTransactionManagement
 @Slf4j
 @RequiredArgsConstructor
