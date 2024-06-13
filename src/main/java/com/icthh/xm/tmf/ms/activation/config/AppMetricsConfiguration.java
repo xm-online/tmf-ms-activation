90e7cd9d74a03c65fecb0a756245da6e796c75e5
Roman Ternovyi <rternovyi@itsmartflex.com>
2024-06-13 17:00:06 +0300

    feat(metrics): remove redundant metrics
    Reasoning: this configuration of metrics results in each instance of activation microservice query single database, which is redundant. This kind of metrics should be configured in a single standalone component, that will query database only once.

--- src/main/java/com/icthh/xm/tmf/ms/activation/config/AppMetricsConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/activation/config/AppMetricsConfiguration.java
@@ -1,31 +1,14 @@
 package com.icthh.xm.tmf.ms.activation.config;
 
-import static com.icthh.xm.tmf.ms.activation.domain.SagaEvent.SagaEventStatus.ON_RETRY;
-import static com.icthh.xm.tmf.ms.activation.domain.SagaEvent.SagaEventStatus.SUSPENDED;
-import static com.icthh.xm.tmf.ms.activation.domain.SagaTransactionState.NEW;
-import static java.time.Instant.now;
-
-import com.codahale.metrics.Gauge;
-import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.MetricSet;
-import com.icthh.xm.commons.config.client.repository.TenantListRepository;
-import com.icthh.xm.tmf.ms.activation.repository.SagaEventRepository;
-import com.icthh.xm.tmf.ms.activation.repository.SagaTransactionRepository;
-import com.icthh.xm.tmf.ms.activation.utils.TenantUtils;
 import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
 import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 import com.zaxxer.hikari.HikariDataSource;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
 import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @EnableMetrics(proxyTargetClass = true)
@@ -33,28 +16,15 @@ public class AppMetricsConfiguration extends MetricsConfigurerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(AppMetricsConfiguration.class);
 
-    private final SagaTransactionRepository sagaTransactionRepository;
-    private final SagaEventRepository sagaEventRepository;
     private final MetricRegistry metricRegistry;
-    private final ApplicationProperties applicationProperties;
     private final ActivationKafkaOffsetsMetric kafkaOffsetsMetric;
-    private final TenantListRepository tenantListRepository;
-    private final TenantUtils tenantUtils;
 
     private HikariDataSource hikariDataSource;
 
-    public AppMetricsConfiguration(@Lazy SagaTransactionRepository sagaTransactionRepository,
-                                   @Lazy SagaEventRepository sagaEventRepository, MetricRegistry metricRegistry,
-                                   TenantListRepository tenantListRepository,
-                                   ApplicationProperties applicationProperties, ActivationKafkaOffsetsMetric kafkaOffsetsMetric,
-                                   TenantUtils tenantUtils) {
-        this.sagaTransactionRepository = sagaTransactionRepository;
-        this.sagaEventRepository = sagaEventRepository;
+    public AppMetricsConfiguration(MetricRegistry metricRegistry,
+                                   ActivationKafkaOffsetsMetric kafkaOffsetsMetric) {
         this.metricRegistry = metricRegistry;
-        this.applicationProperties = applicationProperties;
         this.kafkaOffsetsMetric = kafkaOffsetsMetric;
-        this.tenantListRepository = tenantListRepository;
-        this.tenantUtils = tenantUtils;
     }
 
     @Autowired(required = false)
@@ -72,28 +42,5 @@ public class AppMetricsConfiguration extends MetricsConfigurerAdapter {
             hikariDataSource.setMetricsTrackerFactory(null);
             hikariDataSource.setMetricRegistry(metricRegistry);
         }
-
-        Map<String, Metric> metrics = new HashMap<>();
-        tenantListRepository.getTenants().stream().map(String::toUpperCase).forEach(tenant -> {
-            metrics.put("transactions.wait." + tenant, inTenant(this::getWaitTransactionsCount, tenant));
-            metrics.put("transactions.all." + tenant, inTenant(sagaTransactionRepository::count, tenant));
-            metrics.put("tasks.onretry." + tenant, inTenant(() -> sagaEventRepository.countByStatus(ON_RETRY), tenant));
-            metrics.put("tasks.suspended." + tenant, inTenant(this::getCountSuspendedTasks, tenant));
-        });
-        metricRegistry.register("com.icthh.xm.tmf.ms.activation", (MetricSet) () -> metrics);
-    }
-
-    private Long getCountSuspendedTasks() {
-        Instant date = now().minusSeconds(applicationProperties.getExpectedTransactionCompletionTimeSeconds());
-        return sagaEventRepository.countByStatusAndCreateDateBefore(SUSPENDED, date);
-    }
-
-    private long getWaitTransactionsCount() {
-        Instant date = now().minusSeconds(applicationProperties.getExpectedTransactionCompletionTimeSeconds());
-        return sagaTransactionRepository.countByCreateDateBeforeAndSagaTransactionState(date, NEW);
-    }
-
-    private Gauge inTenant(Supplier<Long> metricSupplier, String tenant) {
-        return () -> tenantUtils.doInTenantContext(metricSupplier::get, tenant);
     }
 }
