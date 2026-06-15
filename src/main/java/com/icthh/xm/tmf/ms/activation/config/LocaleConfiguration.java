343a1c728860d1234dfcc089daa946b0d1002217
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-06-15 18:55:16 +0300

    Clean up;

--- src/main/java/com/icthh/xm/tmf/ms/activation/config/LocaleConfiguration.java
+++ src/main/java/com/icthh/xm/tmf/ms/activation/config/LocaleConfiguration.java
@@ -1,8 +1,6 @@
 package com.icthh.xm.tmf.ms.activation.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
