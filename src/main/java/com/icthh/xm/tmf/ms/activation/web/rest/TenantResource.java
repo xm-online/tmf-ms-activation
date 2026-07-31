41e2be763e0e83e1b86f3328c5a0284a6ad4dc1b
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-07-31 14:17:53 +0300

    Add api create tenant;

---
+++ src/main/java/com/icthh/xm/tmf/ms/activation/web/rest/TenantResource.java
@@ -0,0 +1,61 @@
+package com.icthh.xm.tmf.ms.activation.web.rest;
+
+import com.icthh.xm.commons.gen.api.TenantsApiDelegate;
+import com.icthh.xm.commons.gen.model.Tenant;
+import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
+import com.icthh.xm.commons.tenantendpoint.TenantManager;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TenantResource implements TenantsApiDelegate {
+
+    private final TenantManager tenantManager;
+
+    @Override
+    @PreAuthorize("hasPermission({'tenant':#tenant}, 'ACTIVATION.TENANT.CREATE')")
+    @PrivilegeDescription("Privilege to add a new activation tenant")
+    public ResponseEntity<Void> addTenant(Tenant tenant) {
+        tenantManager.createTenant(tenant);
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @PreAuthorize("hasPermission({'tenantKey':#tenantKey}, 'ACTIVATION.TENANT.DELETE')")
+    @PrivilegeDescription("Privilege to delete activation tenant by tenantKey")
+    public ResponseEntity<Void> deleteTenant(String tenantKey) {
+        tenantManager.deleteTenant(tenantKey);
+        return ResponseEntity.ok().build();
+    }
+
+    @Override
+    @PostAuthorize("hasPermission(null, 'ACTIVATION.TENANT.GET_LIST')")
+    @PrivilegeDescription("Privilege to get all activation tenants")
+    public ResponseEntity<List<Tenant>> getAllTenantInfo() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @PostAuthorize("hasPermission({'returnObject': returnObject.body}, 'ACTIVATION.TENANT.GET_LIST.ITEM')")
+    @PrivilegeDescription("Privilege to get activation tenant")
+    public ResponseEntity<Tenant> getTenant(String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @PreAuthorize("hasPermission({'tenant':#tenant, 'state':#state}, 'ACTIVATION.TENANT.UPDATE')")
+    @PrivilegeDescription("Privilege to update activation tenant")
+    public ResponseEntity<Void> manageTenant(String tenant, String state) {
+        tenantManager.manageTenant(tenant, state);
+        return ResponseEntity.ok().build();
+    }
+}
